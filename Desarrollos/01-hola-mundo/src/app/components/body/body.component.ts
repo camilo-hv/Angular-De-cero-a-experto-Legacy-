27ae779c500058caf995284d89a899cfd7d7abcb
Camilo Henao <camivallejo2010@hotmail.com>
2024-11-02 22:51:08 -0500

    Funcionamiento *ngIf y *ngFor

--- Desarrollos/01-hola-mundo/src/app/components/body/body.component.ts
+++ Desarrollos/01-hola-mundo/src/app/components/body/body.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
+
 export class BodyComponent {
+  
+  mostrar = true;
+
+  frase:any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  }
+
+  personajes: string[] = ['Ironman','Capitan America','Thor']
 
 }
