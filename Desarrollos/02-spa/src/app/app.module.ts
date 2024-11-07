3aa3bc9c9c6a064a5928c213fb25f7bb6625f277
Camilo Henao <camivallejo2010@hotmail.com>
2024-11-06 23:01:49 -0500

    Condiguración SPA

--- Desarrollos/02-spa/src/app/app.module.ts
+++ Desarrollos/02-spa/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BodyComponent } from './components/shared/body/body.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    BodyComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
